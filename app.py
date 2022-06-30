123866dfd7143b5c34ba7fce7270383f8ed360d7
DtugHien <kgha2099@gmail.com>
2022-06-30 08:56:38 +0700

    test heroku

--- app.py
+++ app.py
@@ -110,6 +110,6 @@ def index():
 #     print(last_date, begin_date)
 #     return "Filter"
 
-
+port = int(os.environ.get("PORT", 5000))
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='6868')
+    app.run(debug=True, host='0.0.0.0', port=port)
